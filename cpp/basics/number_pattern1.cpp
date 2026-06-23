047e1d5f0c78196397c5b4526e9552a7ee47676f
sshivi2507 <sshivi2507@gmail.com>
2026-06-23 10:56:49 +0530

    pattern print1

---
+++ cpp/basics/number_pattern1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   for(int j=0;j<5;j++)
+   {
+      for(int i=0;i<=j;i++)
+       cout<<i+1;
+       cout<<"\n";
+   }
+    return 0; 
+}
