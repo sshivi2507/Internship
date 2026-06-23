0141a264a49b76dfe4779c29f0ffbd39ce0e3947
sshivi2507 <sshivi2507@gmail.com>
2026-06-23 11:01:51 +0530

    star print

---
+++ cpp/basics/star_pattern1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int j=0;j<5;j++)
+    {
+        for(int i=0;i<5;i++)
+        cout<<"*";
+        cout<<"\n";
+        
+    }
+   return 0;
+}
